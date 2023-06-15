7b9e151fbf79f55d61fa211061fe9c402c7da427
matosr96 <19matos96@gmail.com>
2023-06-14 19:58:22 -0500

    update

--- cotizador-rebranded/src/routes/customer.routes.js
+++ cotizador-rebranded/src/routes/customer.routes.js
@@ -97,7 +97,7 @@ router.post("/", [verifyToken, customerCtrl.createCustomer])
  *      500:
  *        description: Server error
  */
-router.get("/", [ customerCtrl.getCustomersByUserId])
+router.get("/", [customerCtrl.getCustomersByUserId])
 
 /**
  * @swagger
@@ -125,7 +125,7 @@ router.get("/", [ customerCtrl.getCustomersByUserId])
  *      500:
  *        description: Server error
  */
-router.get("/:id", [customerCtrl.getCustomerById])
+router.get("/:id", [verifyToken, customerCtrl.getCustomerById])
 
 /**
  * @swagger
