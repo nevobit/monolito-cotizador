b4d199d7e92b72daf3f4134175b1f9294de4b125
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2023-07-25 15:03:15 -0500

    chore: change image route

--- cotizador-rebranded/src/controllers/product.cotroller.js
+++ cotizador-rebranded/src/controllers/product.cotroller.js
@@ -104,7 +104,7 @@ const getStock = async (req, res) => {
     return res.status(200).json(stock)
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ message: 'Something went wrong' })
+    return res.status(400).json({ message: error })
   }
 }
 
